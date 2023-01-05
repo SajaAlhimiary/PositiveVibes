71d157a22e9abc89cc5fe0625d2d36b5800820bb
Manal <mw99_a@hotmail.om>
2023-01-05 10:50:10 +0300

    Initial Commit

---
+++ MotivationApp/MotivationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationAppApp.swift
+//  MotivationApp
+//
+//  Created by Manal alwayeli on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MotivationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
